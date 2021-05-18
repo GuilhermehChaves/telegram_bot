6e27ff2542c2577b4ffd4ae5e145c784014c91b1
guilherme <contato.guilhermehchaves@gmail.com>
2021-05-18 12:21:27 -0300

    adds commands

--- bot/__init__.py
+++ bot/__init__.py
@@ -9,31 +9,45 @@ class TelegramBot:
     def __init__(self, token):
         self.token = token
         self.base_url = f'https://api.telegram.org/bot{self.token}'
+        self.commands = {}
 
     def send_message(self, msg: Message, chat_id: str) -> None:
         msg.send(chat_id, self.base_url)
 
-    def run(self) -> None:
-        while True:
-            update_messages = Updates.get_update_messages(self.base_url)
-            latest_update_id = ""
+    def add_command(self, name, action) -> None:
+        self.commands[name] = action
 
-            try:
+    @staticmethod
+    def get_user_mention(user_id, user_name) -> str:
+        return f'<a href="tg://user?id={user_id}">@{user_name}</a>'
+
+    def run(self) -> None:
+        keys = self.commands.keys()
+
+        try:
+            while True:
+                update_messages = Updates.get_update_messages(self.base_url)
+                latest_update_id = ""
+
                 for i, item in enumerate(update_messages['result']):
                     if i != 0:
-                        chat_id = item['message']['chat']['id']
-                        user_id = item['message']['from']['id']
-                        user_name = item['message']['from']['first_name']
                         user_message = item['message']['text']
 
-                        bot_message = f'<a href="tg://user?id={user_id}">@{user_name}</a> {user_message}'
+                        if user_message in keys:
+                            action = self.commands[user_message]
+                            action(item)
+                        else:
+                            chat_id = item['message']['chat']['id']
+                            user_id = item['message']['from']['id']
+                            user_name = item['message']['from']['first_name']
 
-                        self.send_message(TextMessage(bot_message), chat_id)
+                            bot_message = f'<a href="tg://user?id={user_id}">@{user_name}</a> {user_message}'
+                            self.send_message(TextMessage(bot_message), chat_id)
 
                         latest_update_id = item['update_id']
 
-            except KeyboardInterrupt:
-                sys.exit()
+                Updates.clear_replied_messages(self.base_url, latest_update_id)
+                time.sleep(0.5)
 
-            Updates.clear_replied_messages(self.base_url, latest_update_id)
-            time.sleep(0.5)
+        except KeyboardInterrupt:
+            print("Interrupting")
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from bot import TelegramBot
+from bot.message import TextMessage
+
+telegram_bot = TelegramBot('BOT_TOKEN')
+
+
+def start(update):
+    chat_id = update['message']['chat']['id']
+    user_id = update['message']['from']['id']
+    first_name = update['message']['from']['first_name']
+
+    user_mention = TelegramBot.get_user_mention(user_id, first_name)
+
+    telegram_bot.send_message(
+        TextMessage(f'{user_mention} Você está começando!!!'),
+        chat_id
+    )
+
 
 if __name__ == '__main__':
-    telegram_bot = TelegramBot('BOT_TOKEN')
+    telegram_bot.add_command("/start", start)
     telegram_bot.run()
